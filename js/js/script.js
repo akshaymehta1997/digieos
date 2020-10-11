0f3c7b818dfb1bf447f1bd2228dc74e43cf61584
mehtaakshay310 <mehtaakshay310@gmail.com>
2020-10-11 20:48:20 +0530

    testimonial test1

--- js/js/script.js
+++ js/js/script.js
@@ -1,11 +1,48 @@
 // --------------------------------------------------Navbar------------------------------------------------
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".nav-links").classList.toggle("open");
+    document.querySelectorAll(".nav-links li").forEach(link => {
         link.classList.toggle("fade");
     });
 });
+
+function toggleSlidestoShow() {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        document.querySelector(".graphic-design").innerHTML = 
+        `<div class="col-service-content">
+        <div class="col-service-image image-right-lg">
+        <img src="./temp/graphic.jpg">
+        </div>
+        <h1 class="service-header">Graphic design</h1>
+        <div class="service-line "></div>
+        <p>Experts in graphic design, we craft award-winning and cutting-edge online experiences. We build brand stories with each design.</p>
+        </div>`
+        return 1;
+    } else {
+        document.querySelector(".graphic-design").innerHTML = 
+        `<div class="col-service-content">
+        <h1 class="service-header">Graphic design</h1>
+        <div class="service-line "></div>
+        <p>Experts in graphic design, we craft award-winning and cutting-edge online experiences. We build
+            brand
+            stories with each design.</p>
+    </div>
+    <div class="col-service-image image-right-lg"><img src="./temp/graphic.jpg"></div>`
+        return 2;
+    }
+}
+
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: toggleSlidestoShow(),
+    draggable: true,
+    dots: "#dots",
+    arrows: {
+        prev: ".glider-prev",
+        next: ".glider-next"
+    }
+})
+
+window.addEventListener('resize',toggleSlidestoShow)
+
+
