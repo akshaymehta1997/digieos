3c7db9aead6f0a2897fb18c49000742fce2c1791
stgoilkar <stgoilkar@gmail.com>
2020-10-12 00:03:05 +0530

    navbar

--- js/js/script.js
+++ js/js/script.js
@@ -9,8 +9,8 @@ document.querySelector(".hamburger").addEventListener("click", () => {
 
 function toggleSlidestoShow() {
     if (window.matchMedia("(max-width: 768px)").matches) {
-        document.querySelector(".graphic-design").innerHTML = 
-        `<div class="col-service-content">
+        document.querySelector(".graphic-design").innerHTML =
+            `<div class="col-service-content">
         <div class="col-service-image image-right-lg">
         <img src="./temp/graphic.jpg">
         </div>
@@ -18,10 +18,9 @@ function toggleSlidestoShow() {
         <div class="service-line "></div>
         <p>Experts in graphic design, we craft award-winning and cutting-edge online experiences. We build brand stories with each design.</p>
         </div>`
-        return 1;
     } else {
-        document.querySelector(".graphic-design").innerHTML = 
-        `<div class="col-service-content">
+        document.querySelector(".graphic-design").innerHTML =
+            `<div class="col-service-content">
         <h1 class="service-header">Graphic design</h1>
         <div class="service-line "></div>
         <p>Experts in graphic design, we craft award-winning and cutting-edge online experiences. We build
@@ -29,20 +28,47 @@ function toggleSlidestoShow() {
             stories with each design.</p>
     </div>
     <div class="col-service-image image-right-lg"><img src="./temp/graphic.jpg"></div>`
-        return 2;
     }
 }
 
 new Glider(document.querySelector('.glider'), {
-    slidesToShow: toggleSlidestoShow(),
+    slidesToShow: 1,
     draggable: true,
     dots: "#dots",
     arrows: {
         prev: ".glider-prev",
         next: ".glider-next"
-    }
+    },
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 2,
+
+        }
+    }]
 })
 
-window.addEventListener('resize',toggleSlidestoShow)
+window.addEventListener('resize', toggleSlidestoShow)
 
+const header = document.querySelector("#navbarScroll");
+const sectionOne = document.querySelector("#serviceScroll");
 
+const sectionOneOptions = {
+    rootMargin: "0px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+        entries,
+        sectionOneObserver
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                header.classList.add("nav-scrolled");
+            } else {
+                header.classList.remove("nav-scrolled");
+            }
+        });
+    },
+    sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
